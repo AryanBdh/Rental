dda7c391d63af30d565c14c6e77d940fe55aeffa
AryanBdh <aryanbdh30@gmail.com>
2025-12-29 15:28:39 +0545

    Connection Changed

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Rental');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to the database');
     } catch (error) {
         console.log('Error connecting to the database');
